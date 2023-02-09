eec0c7180b52e18e61aba695adf28766f162cdb0
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2023-02-10 02:06:28 +0300

    BASE64Test

--- src/test/java/name/krot/crypto/codingTests/BASE64Test.java
+++ src/test/java/name/krot/crypto/codingTests/BASE64Test.java
@@ -1,59 +1,47 @@
 package name.krot.crypto.codingTests;
 
 import lombok.extern.slf4j.Slf4j;
+import name.krot.crypto.coding.BASE32;
 import name.krot.crypto.coding.BASE64;
 import name.krot.crypto.coding.Coder;
 import name.krot.crypto.util.Fish;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
-@SpringBootTest(classes = {BASE64.class})
-class BASE64Test extends BaseCoderTest {
+public class BASE64Test extends BaseCoderTest {
 
-    @Autowired
-    private Coder base64;
+    private static final String CODE_EXAMPLE_BASE64 =
+            "0JrRgNC40L/RgtC+0LPRgNCw0YTQuNGPINC90LAg0L/RgNCw0LrRgtC40LrQtQ==";
+    private final static Coder base64 = new BASE64();
 
     @Test
-    @DisplayName("Корректность декодирования")
-    void testEncodeAndDecode() {
-        String fishLW = Fish.fishLW();
-        String encrypt = base64.encode(fishLW);
-        String decrypt = base64.decode(encrypt);
-        assertEquals(decrypt, fishLW);
+    public void testEncodeAndDecode() {
+        super.testEncodeAndDecode(base64);
     }
 
     @Test
-    @DisplayName("Кодирование")
-    void testEncode() {
-        String encrypt = base64.encode(Fish.cryptographyRU());
-        log.info(encrypt);
+    public void testEncode() {
+        super.testEncode(base64);
     }
 
     @Test
-    @DisplayName("Декодирование")
-    void testDecode() {
-        String decrypt = base64.decode("0JrRgNC40L/RgtC+0LPRgNCw0YTQuNGPINC90LAg0L/RgNCw0LrRgtC40LrQtQ==");
-        log.debug(decrypt);
+    public void testDecode() {
+        super.testDecode(base64, CODE_EXAMPLE_BASE64);
     }
 
-    @Test
-    @DisplayName("Скорость декодирования")
-    void loadTestDecode() {
-        for (int i = 0; i < 1_000_000; i++) {
-            base64.decode("0JrRgNC40L/RgtC+0LPRgNCw0YTQuNGPINC90LAg0L/RgNCw0LrRgtC40LrQtQ==");
-        }
+    @Benchmark
+    public String benchmarkDecode() {
+        return base64.decode(CODE_EXAMPLE_BASE64);
     }
 
-    @Test
-    @DisplayName("Скорость кодирования")
-    void loadTestEncode() {
-        for (int i = 0; i < 1_000; i++) {
-            base64.encode(Fish.cryptographyRU());
-        }
+    @Benchmark
+    public String benchmarkEncode() {
+        return base64.encode(Fish.cryptographyRU());
     }
 }
