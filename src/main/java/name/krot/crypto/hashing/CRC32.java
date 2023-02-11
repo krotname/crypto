247c62446f92cb9f5c5c8fc716fa02aef4e04651
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2023-02-11 11:24:20 +0300

    CRC32 add

---
+++ src/main/java/name/krot/crypto/hashing/CRC32.java
@@ -0,0 +1,10 @@
+package name.krot.crypto.hashing;
+
+public class CRC32 implements Hash {
+    @Override
+    public String hash(String string) {
+        final java.util.zip.CRC32 crc32 = new java.util.zip.CRC32();
+        crc32.update(string.getBytes());
+        return Long.toHexString(crc32.getValue()); // Hexadecimal
+    }
+}
---
+++ src/test/java/name/krot/crypto/hashing/CRC32Test.java
@@ -0,0 +1,33 @@
+package name.krot.crypto.hashing;
+
+import lombok.extern.slf4j.Slf4j;
+import name.krot.crypto.common.BenchmarkTest;
+import name.krot.crypto.util.Fish;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+public class CRC32Test extends BenchmarkTest {
+
+    private final static Hash CRC_32 = new CRC32();
+
+    @Test
+    void hash() {
+        log.info(CRC_32.hash(Fish.cryptographyRU()));
+    }
+
+    @Test
+    void uniquenessTest() {
+        String hash1 = CRC_32.hash(Fish.cryptographyRU());
+        String hash2 = CRC_32.hash(Fish.cryptographyRU());
+        assertEquals(hash1, hash2);
+    }
+
+    @Benchmark
+    public void benchmarkCRC32(Blackhole bh) {
+        bh.consume(CRC_32.hash(Fish.cryptographyRU()));
+    }
+}
