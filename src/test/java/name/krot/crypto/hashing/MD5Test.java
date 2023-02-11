4a2505438c683af89c0d66aa567b8d86332ec047
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2023-02-11 11:09:38 +0300

    uniquenessTest add

--- src/test/java/name/krot/crypto/hashing/MD5Test.java
+++ src/test/java/name/krot/crypto/hashing/MD5Test.java
@@ -1,11 +1,12 @@
 package name.krot.crypto.hashing;
 
 import lombok.extern.slf4j.Slf4j;
-import name.krot.crypto.hashing.Hash;
-import name.krot.crypto.hashing.MD5;
+import name.krot.crypto.util.Fish;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @SpringBootTest(classes = {MD5.class})
@@ -16,6 +17,13 @@ class MD5Test {
 
     @Test
     void hash() {
-        log.info(mD5.hash("555555555")); // todo подругому
+        log.info(mD5.hash(Fish.cryptographyRU())); // todo подругому
+    }
+
+    @Test
+    void uniquenessTest() {
+        String hash1 = mD5.hash(Fish.cryptographyRU());
+        String hash2 = mD5.hash(Fish.cryptographyRU());
+        assertEquals(hash1, hash2);
     }
 }
